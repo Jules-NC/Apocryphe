408a69b868852395162cbb65ca8c9b2833032591
Lambyor <lambyor@gmail.com>
2017-09-09 17:47:13 +0200

    Rectification commit repassage sur beautifulsoup pour cette branche que avanc ca a fait npqr

--- lapierousse/larousse_trad.py
+++ lapierousse/larousse_trad.py
@@ -1,31 +1,10 @@
 # -*- coding: utf-8 -*-
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 import urllib.request
 
 
-class traduction_parser(HTMLParser):
-    def __init__(self, targeted_tag):
-        super().__init__()
-        self.targeted_tag = targeted_tag
-        self.in_tag = False
-
-    def handle_starttag(self, tag, attrs):
-        if len(tag) is not 0:
-            if ('class', self.targeted_tag) in attrs:
-                self.in_tag = True
-        pass
-
-    def handle_endtag(self, tag):
-        self.in_tag = False
-        pass
-
-    def handle_data(self, data):
-        if self.in_tag:
-            print(data)
-        pass
 
 if __name__ == '__main__':
-    parser = traduction_parser('numero')
-    u = urllib.request.urlopen('http://www.larousse.fr/dictionnaires/anglais-francais/plant')
-    data = u.read().decode('utf8')
-    parser.feed(data)
+    data = urllib.request.urlopen('http://www.larousse.fr/dictionnaires/anglais-francais/plant').read().decode('utf8')
+    soup = BeautifulSoup(data, 'html.parser')
+    print(soup.prettify())
