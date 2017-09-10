9ceba5c98584c1bf5eb785930a19ea40b0245073
Lambyor <lambyor@gmail.com>
2017-09-10 02:02:46 +0200

    Modification de larousse_synset Ajout de (je pense) toutes les fonctions permettant de créer un Synset progressivement, en même temps que le futur parser Code parfait pour l'instant dans sa syntaxe

--- lapierousse/larousse_synset.py
+++ lapierousse/larousse_synset.py
@@ -1,33 +1,60 @@
-import re
+"""LarousseSynset provide a good way to interprete Larousse.fr en-fr traductions and to convert them to a real good
+database"""
 
 
 class LarousseSynset:
     def __init__(self):
-        self.adress = None
+        self.name = None
         self.gramatical_category = None
-        self.number = None
         self.meanings = []
+
+    def last_meaning(self):
+        if len(self.meanings) is not 0:
+            return self.meanings[-1]
+        return None
+
+    def add_number(self, new_number):
+        new_meaning = Meaning()
+        new_meaning.number = new_number
+        self.meanings.append(new_meaning)
+
+    def set_traduction(self, new_traduction, new_metadatas):
+        try:
+            self.last_meaning().traduction = Traduction(new_traduction, Metadata(new_metadatas))
+        except AttributeError:  # If last meaning doesn't exist
+            raise AttributeError  # TODO: delete this error test line
+            # return    TODO: remove commentary after removed precedent line
+
+    def add_example(self, new_example, new_metadatas):
+        try:
+            self.last_meaning().examples.append(Example(new_example, Metadata(new_metadatas)))
+        except AttributeError:  # If last meaning doesn't exist
+            raise AttributeError  # TODO: delete this error test line
+            # return    TODO: remove commentary after removed precedent line
 
 
 class Meaning:
     def __init__(self):
-        self.traduction = Traduction()
-        self.exemple = Example()
+        self.number = None
+        self.traduction = None
+        self.examples = []
 
 
 class Traduction:
-    def __init__(self):
-        self.traduction = None
-        self.metadatas = None
+    def __init__(self, new_traduction, new_metadata):
+        self.traduction = new_traduction
+        self.metadatas = new_metadata
 
 
 class Example:
-    def __init__(self):
-        self.example = None
-        self.example_trad = None
+    def __init__(self, new_example, new_metadatas):
+        self.example = new_example.raw
+        self.example_trad = new_example.trad
+        self.metadatas = new_metadatas
+
 
 class Metadata:
-    def __init__(self):
-        self.domain = None
-        self.metalang = None
-        self.category = None
+    def __init__(self, new_metadatas):
+        self.domain = new_metadatas.domain
+        self.metalang = new_metadatas.metalang
+        self.category = new_metadatas.category
