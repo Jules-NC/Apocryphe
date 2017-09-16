7a69b93ecacd0e158f04ceda9707b5c3dc83c697
Lambyor <lambyor@gmail.com>
2017-09-16 23:19:38 +0200

    Avancement du module de gestion de bdd Ajout de merge qui accepte un nombre infini de sources et une target Marche impec ^^

--- database/wordsfinder.py
+++ database/wordsfinder.py
@@ -24,21 +24,24 @@ def normalize(word):
     return word  # Ouah ! Perfect word
 
 
-def no_duplicates(source):  # TODO: Régler le pb des espaces
+def clean_file(source):
     with open('../ressources/databases/' + source, 'r') as f:
         words_set = set()
         for word in f:
             word = normalize(word)
-            words_set.add(word + '\n')
+            if len(word) > 0:
+                words_set.add(word + '\n')
 
     with open('../ressources/databases/' + source, 'w') as f:
         for word in words_set:
             f.write(word)
 
 
-def merge(source, target):
+def simple_merge(source, target):
     words_file_1 = set()
     words_file_2 = set()
+
+    clean_file(source)
 
     with open('../ressources/databases/' + source, 'r') as s:
         for line in s:
@@ -47,15 +50,35 @@ def merge(source, target):
     with open('../ressources/databases/' + target, 'r') as t:
         for line in t:
             words_file_2.add(line)
-
     res = words_file_1.union(words_file_2)
 
     with open('../ressources/databases/c.txt', 'w') as f:    # TODO: temporaire (fichier source plus tard
         for word in res:
-            print(word)
-            f.write(word)   # TODO: vérifier \n
+            f.write(word)
+
+
+def merge(target, sources):  # Sources is iterator
+    words_sources = set()
+    words_target = set()
+
+    for source in sources:
+        clean_file(source)
+        with open('../ressources/databases/' + source, 'r') as s:
+            for word in s:
+                words_sources.add(word)
+
+    with open('../ressources/databases/' + target, 'r') as t:
+        for word in t:
+            words_target.add(word)
+
+    all_words = words_target.union(words_sources)
+
+    with open('../ressources/databases/' + target, 'w') as t:
+        for word in all_words:
+            t.write(word)
 
 
 if __name__ == '__main__':
-    no_duplicates('a.txt')
-    merge('a.txt', 'b.txt')
+    #clean_file('a.txt')
+    #simple_merge('a.txt', 'b.txt')
+    merge('c.txt', ('a.txt', 'b.txt'))
