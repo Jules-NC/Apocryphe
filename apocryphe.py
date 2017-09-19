9389738e048a02ca50065f3f21cb383bf82b65f0
Lambyor <lambyor@gmail.com>
2017-09-19 23:14:40 +0200

    random pondéré ajouté

--- apocryphe.py
+++ apocryphe.py
@@ -8,13 +8,33 @@ class Apocryphe:
             self.corpus = pickle.load(f)
             pass
 
-        self.corpus = list(self.corpus)
+        self.corpus = dict_to_list_with_tuples(self.corpus)
+        random.shuffle(self.corpus)
+        self.corpus = self.corpus[0:100]
+
+    def print(self):
+        for duo in self.corpus:
+            for key, _ in duo:
+                print(key)
+
+    def select(self):
+        return random.choice(self.corpus)
 
 
+def dict_to_list_with_tuples(dict_):
+    return [(key, dict_[key]) for key in dict_]
 
-def dict_to_list_with_tuples():
-    pass
 
+def random_pond(l):
+    # Init
+    chosen_number = random.randint(0, sum(l)-1)
+    i = 0
+    # Continuation
+    while chosen_number > 0:
+        chosen_number -= l[i]
+        i += 1
+    # End
+    return i-1
 
 
 a = Apocryphe()
