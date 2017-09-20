69e245280505369ef4509d46103e92699547de8b
Lambyor <lambyor@gmail.com>
2017-09-20 11:16:12 +0200

    'le sequelette' GUI presque terminé.

--- gui.py
+++ gui.py
@@ -7,8 +7,12 @@ Flèche solaire (quoique ca veuille dire)
 """
 # from pycallgraph.output import GraphvizOutput   # TODO: vérifier les importations
 # from pycallgraph import PyCallGraph
+from collections import namedtuple
 # from termcolor import colored
 from apocryphe import *
+
+
+CONTINUATION = True
 
 
 def main():
@@ -18,58 +22,79 @@ def main():
     except FileNotFoundError:  # If first time
         gui = GUI()
 
-    gui.init()  # Begin the main process
+    gui.main()  # Begin the main process
 
 
 class GUI:
     def __init__(self):
-        self.apocryphes = [Apocryphe()]
+        self.apocryphes = {'main':Apocryphe()}
         self.files_nicknames = {'main':construct_filename()}
         self.current_branch = 'main'
         self.loaded_apos = ['main']
 
-
-        # self.commands = {
-        #         'print': print(apo),
-        #         'quit': None,  # TODO: créer une fonction arret qui modifie un arg global
-        #         'reset': apo.reset,  # Oui
-        #         'save': apo.save,  # Oui TODO: nommage de la save
-        #         'import': None,  # TODO: import via un nom
-        #         'sort': apo.sort,  # TODO: facile à faire
-        #         'sorta': apo.sort_alphabetical,  # TODO: izi
-        #         'squit': apo.s_quit,  # TODO: ridicule, mais je vais le faire
-        #         'unlock': apo.unlock,  # TODO: izi
-        #         'lock': lethe.lock # TODO: en fait ca c'est eazy mais bon...
-        #         }
-
     def save(self, nickname):
-        pass
+        with open(self.files_nicknames[nickname], 'wb') as f:
+            pickle.dump(self.apocryphes[nickname])
 
     def delete(self, nickname):
-        pass
+        self.unload(nickname)
+        del self.files_nicknames[nickname]
 
     def add(self, filename, nickname):
-        pass
+        try:
+            with open(filename, 'r') as f:  # If the file exists
+                self.files_nicknames[nickname] = filename
+        except FileNotFoundError:
+            print('ERREUR: Robinson_Crusoé')
 
     def load(self, nickname):
-        pass
+        with open(self.files_nicknames[nickname], 'rb') as f:
+            self.apocryphes[nickname] = pickle.load(f)
+            self.loaded_apos += nickname
 
     def unload(self, nickname):
-        pass
+        if self.current_branch == nickname:
+            self.current_branch = None
+        self.loaded_apos.remove(nickname)
+        del self.apocryphes[nickname]
 
     def branch(self, nickname):
-        pass
+        if nickname in self.loaded_apos:
+            self.current_branch = nickname
+        else:
+            print('ERREUR: Nelson_Mandella')
 
-    def merge(self, target, nickname):
+    def merge(self, target, nickname):  # TODO: ca
         pass
 
     def savestate(self):
         with open('usr/core_savestate.pkl', 'wb') as f:
             pickle.dump(self, f)
 
-    def init(self):
-        print('TEST_INITIALISATION')
-        self.savestate()
+    def main(self):
+        # apo = self.apocryphes[self.current_branch]
+        commands = {
+                'delete': self.delete,
+                'add': self.add,
+                'load': self.load,
+                'unload': self.unload,
+                'branch': self.branch,
+                'merge': self.merge,
+                'savestate': self.savestate,
+                'quit': continuation_off,
+                'train': self.train
+                }
+
+        while CONTINUATION:
+            entry = command_parser(input('>: '))
+            if entry.root in commands:
+                try:
+                    commands[entry.root](*entry.args)
+                except TypeError:
+                    print('ERREUR: Jacquouille_la_Fripouille')
+
+    def train(self):
+        pass
 
     # def init(self):
     #     # INITIALISATION
@@ -83,10 +108,25 @@ class GUI:
     #         entry = input(">:")
 
 
+Command = namedtuple('Command', ['root', 'args'])
+
+
+def command_parser(entry):
+    entry = entry.split(' ')
+    if len(entry[0]) is 0:
+        return Command(None, [None])
+    return Command(entry[0], entry[1:])
+
+
 def construct_filename():
     now = datetime.datetime.now()
     date = now.strftime("_%Y-%m-%d|%H:%M:") + str(now.second)
     return 'usr/apocryphes/apo_' + date + '.pkl'
+
+
+def continuation_off():
+    global CONTINUATION
+    CONTINUATION = False
 
 
 def clr_screen(i):
